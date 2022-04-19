bc1f5f3877e153bb0561ac5e857670de79430abf
someshkovi <someshkovi@gmail.com>
2022-04-19 13:05:32 +0530

    removed db from git and media root

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -19,12 +19,12 @@ CORE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*fuc=(2#qh2hb#r0$2o*c(g#sx$p=p8(7uby#41%v0z=$1k80&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('SECRET_KEY', True)
 
-ALLOWED_HOSTS = ['someshkovi.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '152.67.161.19']
 
 
 # Application definition
