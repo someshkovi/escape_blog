7fd47bd7bf3b93d60a3c6f9ba5f2968704325897
someshkovi <someshkovi@gmail.com>
2022-04-19 14:33:07 +0530

    debug toolbar and env files

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -19,12 +19,12 @@ CORE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'test-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('SECRET_KEY', True)
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '152.67.161.19']
+ALLOWED_HOSTS = ['.pythonanywhere.com', '152.67.161.19']
 
 
 # Application definition
@@ -186,3 +186,15 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
+
+
+if DEBUG:
+    INSTALLED_APPS+=['debug_toolbar']
+
+    MIDDLEWARE+=['debug_toolbar.middleware.DebugToolbarMiddleware']
+
+    ALLOWED_HOSTS+=['127.0.0.1']
+
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
